578b3e12b7ca3c53f4310d4a91200ce0e92b2404
mumei <lesilluminations0@gmail.com>
2022-11-18 17:09:59 +0900

    .

--- app/Main.hs
+++ app/Main.hs
@@ -9,5 +9,11 @@ zeroIndexArray = array (0,9) [(3,True)]
 oneIndexArray :: UArray Int Bool
 oneIndexArray = array (1,10) $ zip [1 .. 10] $ cycle [True]
 
+beansInBuckets :: UArray Int Int
+beansInBuckets = array (0,3) []
+
+updatedBiB :: UArray Int Int
+updatedBiB = beansInBuckets // [(1,5),(3,6)]
+
 main :: IO ()
 main = someFunc
