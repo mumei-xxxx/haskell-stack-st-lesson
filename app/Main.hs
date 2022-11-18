5d2b1486c9abedb2057061058c6b5c20cd13ee13
mumei <lesilluminations0@gmail.com>
2022-11-18 16:59:57 +0900

    .

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
 import Lib
+import Data.Array.Unboxed
+
+zeroIndexArray :: UArray Int Bool
+zeroIndexArray = array (0,9) [(3,True)]
+
+oneIndexArray :: UArray Int Bool
+oneIndexArray = array (1,10) $ zip [1 .. 10] $ cycle [True]
 
 main :: IO ()
 main = someFunc
