c2e7ccc9e68337fb96134878353bfc061553ff30
mumei <lesilluminations0@gmail.com>
2022-11-18 21:21:47 +0900

    .

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,9 @@ module Main (main) where
 
 import Lib
 import Data.Array.Unboxed
+import Data.Array.ST
+import Control.Monad
+import Control.Monad.ST
 
 zeroIndexArray :: UArray Int Bool
 zeroIndexArray = array (0,9) [(3,True)]
@@ -14,6 +17,18 @@ beansInBuckets = array (0,3) []
 
 updatedBiB :: UArray Int Int
 updatedBiB = beansInBuckets // [(1,5),(3,6)]
+
+listToSTUArray :: [Int] -> ST s (STUArray s Int Int)
+listToSTUArray vals = do
+    let end =  length vals - 1
+    myArray <- newArray (0,end) 0
+    forM_ [0 .. end] $ \i -> do
+        let val = vals !! i
+        writeArray myArray i val
+    return myArray
+
+listToUArray :: [Int] -> UArray Int Int
+listToUArray vals = runSTUArray $ listToSTUArray vals
 
 main :: IO ()
 main = someFunc
